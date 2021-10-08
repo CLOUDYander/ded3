9e0fdf86211c106ec196804b075b035e7bd45b54
Yander <85867084+CLOUDYander@users.noreply.github.com>
2021-10-08 21:49:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function plus_button()
 {let p= document.getElementById("V_Center1").value;
 let b = "Чокола";
 if (p.toUpperCase() === b.toUpperCase()){
-	alert("Ссылка бля");
+	alert("Отправьте этому человеку сообщение (Отчасти почасовой) https://vk.com/thisisyander ");
 }}
